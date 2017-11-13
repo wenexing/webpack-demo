c7579a89b63b60a7b82b454726570a14967c46e2
frank <fengxuting@qq.com>
2017-11-13 14:29:14 +0800

    管理输出&开发

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from '../assets/icon.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -14,7 +16,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
-  
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   return element;
 }
 
